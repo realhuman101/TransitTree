665c3f508773ae25fcb867dbdbb00289d1f341ae
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-08 22:22:32 +0800

    Add base homepage format

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import '../assets/CSS/Home.css'
+import Tree from '../assets/images/Tree.svg'
 
 function Home() {
   return (
     <>
-      <button>Test</button>
+      <div id="CO2text">
+        <h1 id="CO2tons">500 Tons</h1>
+        <h1>of Carbon Emissions Reduced</h1>
+      </div>
+      <img src={Tree} width='100px'/>
+      <button>Plant Tree</button>
     </>
   )
 }
