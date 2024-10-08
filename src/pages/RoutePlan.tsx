3b0bbf9574d9a9ed54e080975de98329dfa6f4cb
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-09 00:59:19 +0800

    add use route button

--- src/pages/RoutePlan.tsx
+++ src/pages/RoutePlan.tsx
@@ -1,23 +1,32 @@
+
+import { useState } from 'react';
 import '../assets/CSS/RoutePlan.css'
 
 function RoutePlan() {
-  return (
-	<>
-		<div id="routePlan">
-			<h1>Plan Your Route</h1>
-			<form id="RoutePlannerForm" onSubmit={(e) => { e.preventDefault(); }}>
-				<label>From</label>
-				<input type="text" id="routePlanFrom" placeholder='Starting Location'/>
-				<label>To</label>
-				<input type="text" id="routePlanTo" placeholder='Destination'/>
-				<input type="submit" value="Find Fastest Route" />
-			</form>
-		</div>
-		<div id="map">
+	const [routePlanned, setRoutePlanned] = useState(false);
 
-		</div>
-	</>
-  )
+	return (
+		<>
+			<div id="routePlan">
+				<h1>Plan Your Route</h1>
+				<form id="RoutePlannerForm" onSubmit={(e) => { 
+						e.preventDefault(); 
+						setRoutePlanned(true);
+					}}>
+
+					<label>From</label>
+					<input type="text" id="routePlanFrom" placeholder='Starting Location'/>
+					<label>To</label>
+					<input type="text" id="routePlanTo" placeholder='Destination'/>
+					<input type="submit" value="Find Fastest Route" />
+				</form>
+			</div>
+			<div id="map">
+
+			</div>
+			{routePlanned && <button>Use Route</button>}
+		</>
+	)
 }
 
 export default RoutePlan
