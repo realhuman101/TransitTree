7e8a6b6cfd8dac85aa140e6af3d48d84185df4ff
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-11 01:42:12 +0800

    Attempt to add routing

--- src/assets/components/Map.tsx
+++ src/assets/components/Map.tsx
@@ -1,12 +1,55 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import React, { useEffect, useState, useRef } from "react"
+import { MapContainer, TileLayer, LayersControl } from 'react-leaflet'
+import L from 'leaflet'
 
-function Map() {		
+type Props = {
+	route: boolean,
+	startCoords?: Array<number>,
+	endCoords?: Array<number>
+}
+
+function Map({ route = false, startCoords = undefined, endCoords = undefined } : Props) {
+	const [map, setMap] = useState(null);
+  	const [routingMachine, setRoutingMachine] = useState(null);
+
+	const RoutingMachineRef = useRef(null);
+
+	useEffect(() => {
+		if (!map) return
+		if (map && route) {
+			RoutingMachineRef.current = L.Routing.control({
+				position: 'topleft', // Where to position control on map
+				lineOptions: { // Options for the routing line
+				styles: [
+					{
+					color: '#73c97e',
+					},
+				],
+				},
+				waypoints: [startCoords, endCoords],
+			})
+
+			setRoutingMachine(RoutingMachineRef.current)
+		}
+	}, [map]);
+
+	useEffect(() => {
+		if (!routingMachine) return
+		if (routingMachine && route) {
+			routingMachine.addTo(map);
+		}
+	}, [routingMachine])
+
 	return (
 		<div id="map">
-			<MapContainer center={[22.42460, 114.21334]} zoom={50} scrollWheelZoom={true} attributionControl={false}>
-				<TileLayer
-					url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-				/>
+			<MapContainer center={[22.42460, 114.21334]} zoom={50} scrollWheelZoom={true} attributionControl={false} whenCreated={(_map) => {setMap(_map)}}>
+				<LayersControl>
+					<LayersControl.BaseLayer checked name="Map">
+						<TileLayer
+							url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+						/>
+					</LayersControl.BaseLayer>
+				</LayersControl>
 			</MapContainer>
 		</div>
 	)
---
+++ src/assets/components/MapAutocomplete.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MapAutocomplete() {
+  return (
+	<div>MapAutocomplete</div>
+  )
+}
+
+export default MapAutocomplete
