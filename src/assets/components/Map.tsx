e39f63044d50315208f5f753750f4499575e481f
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-10 21:37:32 +0800

    Fixed map

--- src/assets/components/Map.tsx
+++ src/assets/components/Map.tsx
@@ -1,15 +1,15 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
-function Map() {
-  return (
-	<div id="map">
-		<MapContainer center={[22.42460, 114.21334]} zoom={50} scrollWheelZoom={true} attributionControl={false}>
-			<TileLayer
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-		</MapContainer>
-	</div>
-  )
+function Map() {		
+	return (
+		<div id="map">
+			<MapContainer center={[22.42460, 114.21334]} zoom={50} scrollWheelZoom={true} attributionControl={false}>
+				<TileLayer
+					url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+				/>
+			</MapContainer>
+		</div>
+	)
 }
 
 export default Map
