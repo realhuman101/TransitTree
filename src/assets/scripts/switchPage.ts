ec4ff8455b91e3a06eb87b122cfe832cffd3f19f
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-11 19:25:56 +0800

    Fix animation playing on same page

--- src/assets/scripts/switchPage.ts
+++ src/assets/scripts/switchPage.ts
@@ -1,15 +1,22 @@
+import { useLocation } from "react-router";
 import { useLoading } from "../components/LoadingContext";
 
 export default function SwitchPage() {
 	const { setIsLoading } = useLoading();
+	const location = useLocation().pathname;
 
 	const pageTransition = (pageName: string) => {
+		let path = "#/TransitTree/" + pageName + "/";
+		if (pageName == '')
+			path = "#/TransitTree/";
+
+		if ("/" + pageName + "/" == location || (pageName == "" && location == "/"))
+			return
+
 		setIsLoading(true);
 		
 		setTimeout(() => {
-			window.location.href = "#/TransitTree/" + pageName + "/";
-			if (pageName == '')
-				window.location.href = "#/TransitTree/";
+			window.location.href = path;
 
 			setIsLoading(false);
 		}, 500 + 100);
