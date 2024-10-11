29b17a93f5ef15df2a0d2b75c985848f3fbce08c
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-11 19:10:48 +0800

    Add bottom footer nav with icons

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,15 +1,21 @@
-import { useLocation } from 'react-router-dom'
 import Home from '../images/Home.svg'
+import Leaderboard from '../images/Leaderboard.svg'
+import Shop from '../images/Shop.svg'
+import Forest from '../images/Forest.svg'
+import Route from '../images/Route.svg'
 import SwitchPage from '../scripts/switchPage';
 
 function Footer() {
-	const location = useLocation().pathname;
 	const pageTransition = SwitchPage();
 
 	return (
 		<div id="footer">
-			{location != "#/TransitTree" && <button onClick={() => pageTransition('')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Home}/></button>}
-			<p>TransitTree Demo</p>
+			<button onClick={() => pageTransition('')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Home}/></button>
+			<button onClick={() => pageTransition('RoutePlan')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Route}/></button>
+			<button onClick={() => pageTransition('ViewForest')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Forest}/></button>
+			<button onClick={() => pageTransition('Shop')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Shop}/></button>
+			<button onClick={() => pageTransition('')}><img id="homeIcon" width="30px" height="30px" color='#fff' src={Leaderboard}/></button>
+			{/* <p>TransitTree Demo</p> */}
 			<p id="footerCredit"><div>Made by <a href="https://realhuman101.github.io" target="_blank">Valentina Banner</a></div></p>
 		</div>
 	)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,20 +12,22 @@ function Home() {
 
 	useEffect(() => {
 		if (cookies.CO2amt == undefined) {
-			setCookies("CO2amt", 500);
+			setCookies("CO2amt", 500000);
 		}
 
-		setCookies("treesPlanted", cookies.CO2amt/100);
+		setCookies("treesPlanted", cookies.CO2amt/100000);
 	}, [cookies, setCookies]);
 	
 	return (
 		<div id={styles.homepage}>
 			<div id={styles.CO2text}>
-				<h1 id={styles.CO2tons}>{cookies.CO2amt} Tons</h1>
-				<h1>of Carbon Emissions Reduced</h1>
+				<h1 id={styles.CO2tons}>{(cookies.CO2amt).toLocaleString()}</h1>
+				<h1>Tons of Carbon Emissions Reduced</h1>
 			</div>
-			<img src={Tree} width='100px'/>
-			<h3>{cookies.treesPlanted} Trees Planted</h3>
+			<div id={styles.treeBox}>
+				<img src={Tree} width='150px'/>
+				<h3>{(cookies.treesPlanted).toLocaleString()} Trees Planted</h3>
+			</div>
 			<div id={styles.buttons}>
 				<button className={styles.button} onClick={() => {
 					pageTransition('RoutePlan');
