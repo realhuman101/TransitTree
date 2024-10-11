2ff9a768f4cdd82db6f09d8edbb5b70821c7ba52
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-10-11 20:10:31 +0800

    Added leaderboard

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,11 +1,51 @@
+import { useCookies } from 'react-cookie'
 import styles from '../assets/CSS/Leaderboard.module.css'
 
-function Leaderboard() {
-  return (
-	<div id={styles.leaderboard}>
+type StatProp = {
+	"Name": string,
+	"Trees": number
+}
 
-	</div>
-  )
+function Leaderboard() {
+	const [cookie] = useCookies(['treesPlanted']);
+
+	const stats: Array<StatProp> = [
+		{
+			"Name": "Valentina Banner",
+			"Trees": 26
+		},
+		{
+			"Name": "Maya Yan",
+			"Trees": 19
+		},
+		{
+			"Name": "Kylton Kim",
+			"Trees": 9
+		},
+		{
+			"Name": "You",
+			"Trees": cookie.treesPlanted
+		}
+	]
+
+	const person = stats.map((item, i) => (
+		<div className={`${styles.person} ${(item.Name === 'You') ? ` ${styles.self}` : ''}`}>
+			<div>
+				<p className={styles.personStat}>#{i+1}</p>
+				<p className={styles.personName}>{item['Name']}</p>
+			</div>
+			<p className={styles.personTrees}><b>Trees Planted:</b> {item.Trees}</p>
+		</div>
+	));
+
+	return (
+		<div id={styles.leaderboard}>
+			<h1>Leaderboard</h1>
+			<div id={styles.board}>
+				{person}
+			</div>
+		</div>
+	)
 }
 
 export default Leaderboard
